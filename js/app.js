31edf0a172af0d41e10dc3561f8c8013a7dd8dd0
S Moe <dukidude@hotmail.com>
2015-02-10 00:05:52 -0700

    Added ui-grid module and test grid to index.html

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 (function(){
-  var app = angular.module('store', ['store-products'])
+  var app = angular.module('call-me-back', ['store-products', 'ui.grid', 'ui.grid.edit', 'ui.grid.rowEdit', 'ui.grid.cellNav'])
   .config( [
     '$compileProvider',
     function( $compileProvider ) {
@@ -16,6 +16,38 @@
   //-------------
   // Controllers
   //-------------
+  app.controller('CallbackController', ['$scope', '$http', '$q', '$interval', function ($scope, $http, $q, $interval) {
+    $scope.gridOptions = [];
+
+    $scope.gridOptions.columnDefs = [
+      { name: 'heat1', 'displayName': 'Heat 1' }
+    ];
+
+    $scope.saveRow = function( rowEntity ) {
+      // TODO: This is a fake promise. Normally need to use the promise returned by $http or $resource
+      var promise = $q.defer();
+      $scope.gridApi.rowEdit.setSavePromise( rowEntity, promise.promise );
+
+      //TODO: This fakes a 1-second delay while saving
+      $interval( function() {
+        if ( isNaN(rowEntity.heat1) ) {
+          promise.reject();
+        } else {
+          promise.resolve();
+        }
+      }, 1000, 1);
+    };
+
+    $scope.gridOptions.onRegisterApi = function(gridApi) {
+      $scope.gridApi = gridApi;
+      gridApi.rowEdit.on.saveRow($scope, $scope.saveRow);
+    };
+
+    $http.get('/data/heat1.json').success(function(data) {
+      $scope.gridOptions.data = data;
+    });
+  }]); // END app.controller('callbacks')
+
   app.controller('StoreController', [ '$http', function($http){
     var store = this;
     store.products = [ ];
